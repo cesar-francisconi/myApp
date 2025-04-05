81fb975fe90ca2ef309806c0e54e89c71daf049e
Cesar Francisconi <cesarafpiza@gmail.com>
2025-04-05 15:23:18 -0300

    initial commit

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import {
+  StyleSheet,
+  View,
+} from 'react-native';
+
+export default function App() {
+  return (
+    <View>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+
+});
